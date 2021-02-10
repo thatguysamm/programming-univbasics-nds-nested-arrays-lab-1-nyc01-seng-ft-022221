401e315f5f8d3a31e709fdd40b501bc481933c9b
friendly-format-0074 <samuelagasi@gmail.com>
2021-02-10 20:04:27 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -21,7 +21,7 @@ def assembled_matrix
 end
 
 def sorted_matrix
-  [ORGANIC_PRODUCE, CONVENTIONAL_PRODUCE].sort
+  [CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE].sort
 end
 
 def matrix_lookup(matrix, row, column)
